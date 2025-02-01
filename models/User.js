80a6bf5259abdcf8bb5e822218db40b84f0979fd
factorgh <burchellsbale@gmail.com>
2025-02-01 20:13:45 +0000

    Added fullName

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema(
   {
     userName: { type: String },
+    fullName: { type: String },
     dateOfBirth: { type: Date },
     gender: {
       type: String,
